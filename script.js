ad773fb4e309847a4c812dfafd4c04950fd5a954
“abraralabbad <AbrarAlabbad192@outlook.com>
2025-09-14 09:18:08 +0300

    Personal Portfolio

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 function init (){
 
 
+const toggleBtn = document.getElementById("toggleMode");
+const body = document.body;
 
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
 
-
-
-
-
+  // Change button icon + text
+  // from stack overflow
+  if (body.classList.contains("light-mode")) {
+    toggleBtn.innerHTML = '<i class="fa-regular fa-moon"></i>';
+    
+  } else {
+    toggleBtn.innerHTML = '<i class="fa-regular fa-lightbulb"></i>';
+  }
+});
 
 
 }
